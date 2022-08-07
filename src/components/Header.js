742f594fa97a9366420c3f482da3da024e32ac91
Luigi Remor <luigiremor@github.com>
2022-08-07 17:25:19 -0300

    Adding header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="h-16 bg-zinc-800 shadow-zinc-900 shadow-xl">
+      <nav className="flex justify-between items-center h-full mx-8">
+        <img
+          className="h-12 "
+          src="https://portal.bridge.ufsc.br/wp-content/uploads/2021/09/cropped-logo_bridge_branca.png"
+        />
+
+        <a>Github</a>
+      </nav>
+    </div>
+  )
+}
