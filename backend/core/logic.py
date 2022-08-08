2a46b15694733a78c96dde472672c0a986d3701f
Luigi Remor <luigiremor@github.com>
2022-08-07 23:32:42 -0300

    Setting django

--- backend/core/logic.py
+++ backend/core/logic.py
@@ -12,12 +12,6 @@ def count_same_divisors(n):
             count += 1
     return count
 
-def main():
 
-    n = int(input("Enter a number: "))
-    print(count_same_divisors(n))
-
-if __name__ == "__main__":
-    main()
 
 
