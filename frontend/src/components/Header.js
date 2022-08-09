aceb9391769b6c047801050eacb70fdbbae6fad3
Luigi Remor <luigiremor@github.com>
2022-08-09 00:10:21 -0300

    Adding href to github repo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,11 +5,17 @@ export default function Header() {
     <div className="h-16 bg-zinc-800 shadow-zinc-900 shadow-md top-0 fixed w-full">
       <nav className="flex justify-between items-center h-full mx-8">
         <img
-          className="h-12 "
+          className="h-12"
+          alt="logo"
           src="https://portal.bridge.ufsc.br/wp-content/uploads/2021/09/cropped-logo_bridge_branca.png"
         />
 
-        <a>Github</a>
+        <a
+          className="hover:text-blue-500"
+          href="https://github.com/luigiremor/desafio-bridge"
+        >
+          Github
+        </a>
       </nav>
     </div>
   )
